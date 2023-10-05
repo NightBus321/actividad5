17a9c8425ef29daf33cbc6f9bbc00a197896a218
NigthBus <sngiagom@gmail.com>
2023-10-05 06:29:44 -0500

    Se crea el main

---
+++ lib/view/LoginView.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:actividad5/controller/UserController.dart';  
+
+class loginUser extends StatelessWidget {
+  final UserController controller = UserController(); 
+  final Color customColor = Color.fromARGB(255, 0, 126, 189);
+} 
